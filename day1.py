9fa6c042ba0a22cacc1b226237fefe232e46c580
Ashini Girupa <ashinigirupa149@gmail.com>
2026-09-10 21:56:51 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,8 @@
+print("Annyeonghaseyo Korea!🇰🇷")
+print("lam Ashini, future developer in South Korea!")
+print("Day1 of my dream journey - started!)
+
+### My Dream Korea                                                                       
+dream = "Work as Developer in Korea"
+print(f"My Dream: {dream}")
+    
