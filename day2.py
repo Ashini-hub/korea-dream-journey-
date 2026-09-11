6d2383d85dd860ce8870b3afe0fe7d8e2298dbd4
Ashini Girupa <ashinigirupa149@gmail.com>
2026-09-11 15:36:35 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,15 @@
+# Day 2 - My Korean Age Calculator 
+#Ashini - 17 years old 
+
+my_age = 17
+korean_age = my_age + 1
+
+print(f"Hello! I'm {my_age} years old")
+print(f" My Korean Age is {korean_age} 살!")
+
+# Numbers I learned
+print("1 - 하나(hana)")
+print("17 - 열일곱(yeolilgop)")
+
+dream = "Developer in Korea"
+print(f"Age {my_age} to {korean_age}, Dream: {dream}")
