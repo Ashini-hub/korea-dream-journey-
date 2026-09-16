1ed68c65a2d6899aa48a9127ecfaadfd2f710053
Ashini Girupa <ashinigirupa149@gmail.com>
2026-09-16 22:43:01 +0530

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,13 @@
+mood = input("Innaikki epdi irukka? (happy/sad/tired):")
+
+if mood == "happy":
+   print("Semma da! Innaikki full joly ah irukka pola!")
+   print("Ippadiye iru, coding kooda easy ah varum!")
+elif mood == "sad":
+   print("Ennachu da? Sad ah irukka?")
+   print("Oru coffee kudichitu, paatu ketu mood maathu da!")
+elif mood == "tired":
+   print("Romba tired ah? Konjam thoongitu va da!")
+   print("Aprom sernthu coding pannalam!")
+else:
+   print("Enna mood nu sariya sollu!")
