b08df01ca435e210e5b54ce89aeb3a4a5ff0b6ce
Ashini Girupa <ashinigirupa149@gmail.com>
2026-09-20 17:46:26 +0530

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,10 @@
+name = input("What is your name?")
+age = int(input("How old are you?"))
+
+print(f"\nHi! {name}!")
+if age<18:
+  print("You are still young, start learning Korean now")
+else:
+  print("Perfect time to chase your korea dream! Go for it!")
+
+          
