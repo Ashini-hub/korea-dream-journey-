109cee6e1a6cfef1e4b8ad251acc9e31c2c5a070
Ashini Girupa <ashinigirupa149@gmail.com>
2026-09-26 21:48:01 +0530

    Create day16.c

---
+++ day16.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+int main() {
+    float f0 = 10; // x=0
+    float f2 = 6*4 - 24*2 + 10; // x=2
+    float f3 = 6*9 - 24*3 + 10; // x=3
+    printf("f(0)=%.2f\nf(2)=%.2f\nf(3)=%.2f\n", f0, f2, f3);
+    printf("Max=10 at x=0, Min=-14 at x=2");
+    return 0;
+}
